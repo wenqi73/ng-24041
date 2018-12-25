6d53de2f7a1da3c5f5f62998c15d62e510a36e6d
wenqi73 <1264578441@qq.com>
2018-12-26 00:54:53 +0800

    chore: find the error version 5.0.0-rc.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  animations: [
+    trigger('tagAnimation', [
+      transition('void => *', [
+        animate('150ms linear', style({ opacity: 1 }))
+      ]),
+      transition('* => void', [
+        animate('150ms linear', style({ opacity: 0 }))
+      ])
+    ])
+  ],
+  template: `
+  <content [display]="display">
+    <span *ngFor="let i of listOfOption" [@tagAnimation]>{{i}}</span>
+  </content>
+  <button (click)="display = !display">toggle</button>
+  `
 })
 export class AppComponent {
   title = 'app';
+  display = true;
+  listOfOption = [1, 2, 3];
+
+  constructor() {
+  }
+
+}
+
+@Component({
+  selector: 'content',
+    animations         : [
+      trigger('dropDownAnimation', [
+    ])
+  ],
+  template: `
+  <div [@dropDownAnimation] *ngIf="display">
+    <ng-content></ng-content>
+  </div>
+  `,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class ContentComponent {
+  @Input() display = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent, ContentComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
